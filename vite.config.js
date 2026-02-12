0b2129d461fc78735199a718374bfa4f254c264b
brandontaylor1 <brandon@thecreativekingdomco.com>
2026-02-11 19:05:58 -0600

    Add .nojekyll for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/thread-emergent/",
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+    publicDir: 'public'
 })
